b0659544b0575c2527f91e35b8ad625797617f63
Mohsen <19164817+damshenas@users.noreply.github.com>
2022-02-18 19:59:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-var apigw = 'https://rnaausivw6.execute-api.us-east-1.amazonaws.com/prod/'
+var apigw = 'https://wr9vlgvlx8.execute-api.us-east-1.amazonaws.com/prod/'
 
 $(document).ready(function () {
   console.log('Heartbeat.');
